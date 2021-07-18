614a5429d53ca66b31f32ec96cfc521bf53ffd47
Mohamed-Mehdi-Gara <garamehdi57@yahoo.com>
2021-07-18 19:57:18 +0100

    added files

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+
+let products = [];
+const app = express();
+
+app.use(express.json());
+app.get("/products", (req, res) => {
+  res.json(products);
+});
+
+app.post("/products", (req, res) => {
+  const body = req.body;
+  products.push(body);
+  res.json(body);
+});
+app.delete("/products/:id", (req, res) => {
+  const id = req.params.id;
+  const filtredProducts = products.filter((value, index) => index != id);
+  products = filtredProducts;
+  res.json(filtredProducts);
+});
+app.patch("/products/:id", (req, res) => {
+  const id = req.params.id;
+  const body = req.body;
+  products[id] = body;
+  res.json(products);
+});
+app.use("/", (req, res) => res.send("Hello world!"));
+
+module.exports = app;
